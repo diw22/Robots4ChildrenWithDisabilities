f696bb5bcb9f009b90fc5780519e783474c3d80f
jinyoungk0126 <jinyoungk0126@gmail.com>
2025-06-02 03:19:33 +0100

    Refactor: Update all packages to ament_cmake and cleaned up workspace

--- ROS2/catkin_ws/build/nav_module/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raspi/Robots4ChildrenWithDisabilities/ROS2/catkin_ws/src/nav_module
-# Build directory: /home/raspi/Robots4ChildrenWithDisabilities/ROS2/catkin_ws/build/nav_module
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
