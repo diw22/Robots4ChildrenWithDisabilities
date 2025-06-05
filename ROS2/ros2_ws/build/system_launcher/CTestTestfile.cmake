72b31a28739693abfc2003c0f7993a707a6af409
jinyoungk0126 <jinyoungk0126@gmail.com>
2025-06-05 06:27:09 +0100

    Add .gitignore to exclude build/install/log and other artifacts

--- ROS2/ros2_ws/build/system_launcher/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/raspi/Robots4ChildrenWithDisabilities/ROS2/ros2_ws/src/system_launcher
-# Build directory: /home/raspi/Robots4ChildrenWithDisabilities/ROS2/ros2_ws/build/system_launcher
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
